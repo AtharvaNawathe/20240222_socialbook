c8ccee0b7bead15d4529d0538b73443a45f3ee6a
Atharva <atharvanhsm@gmail.com>
2024-02-26 11:52:06 +0530

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftSidebarComponent,
+    MainSidebarComponent,
+    RightSidebarComponent
   ],
   imports: [
     BrowserModule,
